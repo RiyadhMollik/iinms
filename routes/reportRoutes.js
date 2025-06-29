85702d2812e32aa67f03a2c9d5f7ee7763cb6e92
tabrid <mollik15-5096@diu.edu.bd>
2025-06-29 12:06:50 +0600

    update

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getLocationCounts } from '../controllers/reportController';
+import { getLocationCounts } from '../controllers/reportController.js';
 
 
 const router = express.Router();
