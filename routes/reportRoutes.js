e3706f782e0aade4cd528ddaa1c7560e34e2b094
tabrid <mollik15-5096@diu.edu.bd>
2025-06-29 12:40:29 +0600

    update

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getLocationCounts } from '../controllers/reportController.js';
+import { getLocationCounts , getBlockCounts } from '../controllers/reportController.js';
 
 
 const router = express.Router();
 
 // Route to get counts of farmers by location type
 router.get('/area-wise-counts', getLocationCounts);
+router.get('/block-wise-counts', getBlockCounts);
 
 export default router;
